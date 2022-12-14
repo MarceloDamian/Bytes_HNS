8f0cbea5e3de22df5943936b6a2c7fb67277f67c
Erick <Erick.Cabrera64@myhunter.cuny.edu>
2022-12-13 23:31:06 -0500

    Map items

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -31,24 +31,53 @@ function Directory({ data }) {
   const [recipes, setRecipes] = useState([]);
   const [alert, setAlert] = useState("");
 
-  const APP_ID = "4e9f05eb";
-  const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
+  const APP_ID = "cc1b9d11";
+  const APP_KEY = "5f8bc8e8f8ee04028d53a121fbb0cd8f";
 
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const url = `https://api.nutritionix.com/v1_1/search/${query}?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=${APP_ID}&appKey=${APP_KEY}`;
+
+  //=`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  //https://api.nutritionix.com/v1_1/search/${query}?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=${APP_ID}&appKey=${APP_KEY}
 
   const getData = async () => {
-    if (query !== "") {
+    if (query !== "") 
+    {
       const result = await Axios.get(url);
-      if (!result.data.more) {
-        return setAlert("No food with such name");
-      }
-      console.log(result);
-      setRecipes(result.data.hits);
-      setQuery("");
-      setAlert("");
-    } else {
+      const items = result.data.hits;
+      const mapitems = items.map 
+      (
+        (item) =>  
+        {
+          // console.log (item.fields.item_name)} 
+          if (!item.fields.item_name)
+          {
+            return setAlert("No food with such name");
+          }
+          console.log(item.fields.item_name);
+          setRecipes(item.fields.item_name);
+          setQuery("");
+          setAlert("");
+          return (
+            <li> Item{item.fields.item_name} </li>
+          );
+        }
+      )
+
+      // if (!result) {
+      //   return setAlert("No food with such name");
+      // }
+      // console.log(result);
+      // setRecipes(result.hits.fields.item_name);
+      // setQuery("");
+      // setAlert("");
+
+    } 
+    else 
+    {
       setAlert("Please fill the form");
     }
+
   };
 
   const onChange = (e) => setQuery(e.target.value);
@@ -61,10 +90,10 @@ function Directory({ data }) {
   return (
     <div className="directory">
       <div className="wrap">
-        <div className="recipes">
+        {/* <div className="recipes">
           {recipes !== [] &&
             recipes.map((recipe) => <Recipe key={uuidv4()} recipe={recipe} />)}
-        </div>
+        </div> */}
         <div className="item" style={{ backgroundImage: `url(${HomePic})` }}>
           {/* Search bar */}
           <h1>
